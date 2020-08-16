6626294dc8c1f20357be84a33ede6de3e01d479d
szuyu <szuyuchen0101@gmail.com>
2020-08-16 23:03:07 +0800

    change data structure and load in real data

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import BarChart from './components/BarChart'
 import {
   identity,
   politics,
+  attitudes,
   independence
 } from './libs/barChartData.json'
 import Footer from './components/Footer';
@@ -27,6 +28,7 @@ const App = () => {
       <BarChart data={identity}/>
       <Text type="center" text={["整體而言有近6成市民支持運動，但兩極化情況明顯——非常支持及非常不支持的市民合共57.2%。若將數字按身份認同及政治立場劃分，可明顯看到支持陣營以香港人、本土派及泛民主派為主；而反對陣營則以混合身份、中國人及建制派為主。"]} />
       <BarChart data={politics}/>
+      <BarChart data={attitudes}/>
       <Intro />
       <BarChart data={independence}/>
       <DotChartA />
