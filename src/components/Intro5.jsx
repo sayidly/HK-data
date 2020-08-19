402caa12e11a380c294c3c877ef6399c453b0a44
sayid <herq12315@gmail.com>
2020-08-19 21:46:32 +0800

    modify intro5 intro text

--- src/components/Intro5.jsx
+++ src/components/Intro5.jsx
@@ -19,15 +19,14 @@ const Intro4 = () => {
     highlightColor: "#ea497b"
   }
 
-  const questionList=["港獨、攬炒、經濟預期、香港未來⋯⋯他們之間的關係是什麼？"];
   const introContent = {
     content: [
+      [<strong>港獨、攬炒、經濟預期、香港未來⋯⋯他們之間的關係是什麼？</strong>],
       ["市民對不同議題的態度之間存在關係嗎？透過交叉分析，我們可以看到經濟預期、香港對中國的重要性、攬炒及港獨之間存在一些相關性，反映了市民對這些議題的看法環環緊扣。"],
   ]}
 
   return (
     <>
-      <Questions class="intro5" questionList={questionList} bgImage={bgImage} />
       <Paragraph class="intro5" introContent={introContent} />
       <LineChartA data={lineData}/>
     </>
